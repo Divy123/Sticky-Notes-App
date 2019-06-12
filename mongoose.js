aa451ce17e95c1e4c6e0a9ea2bf9849b2048dc92
Divy123 <lit2017001@iiitl.ac.in>
2019-06-13 01:41:06 +0530

    update mongoose.js

--- mongoose.js
+++ mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require("mongoose");
+require('dotenv').config();
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/notes-app",{
+mongoose.connect(process.env.MONGOLAB_URI||"mongodb://localhost:27017/notes-app",{
     useCreateIndex:true,
     useNewUrlParser:true
 });
